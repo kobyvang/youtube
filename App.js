5254f49b4b04bf330b7fcf9df078bf0d04824c12
kobyvang <80074281+kobyvang@users.noreply.github.com>
2021-05-19 18:05:37 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+//const axios = require('axios');
+
+function App() {
+  return (
+    <div1 className="App">
+      <header className="YouTube-header">
+        <h1> Welcome to Youtube</h1>
+        <form action="/videos" methos="POST">
+          <input type="text" placeholder="search" name="search"></input>
+          <button href="./App.js">add search picture if possible</button>
+          <a class="home" href="./App.js"> youtube</a>
+        </form>
+        <div2>insert user result videos here</div2>
+      </header>
+    </div1>
+  );
+}
+
+export default App;
